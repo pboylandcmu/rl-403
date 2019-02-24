8b684e7ac47141dba20ab52cf6428fccbfb91948
Peter Boyland <pboyland@andrew.cmu.edu>
2019-02-23 19:14:57 -0500

    I hope this fixed it

--- DQN_implementation.py
+++ DQN_implementation.py
@@ -38,14 +38,7 @@ class QNetwork():
 	def define_model(self,environment_name):
 		model = Sequential()
 		if(environment_name == 'CartPole-v0'):
-<<<<<<< HEAD
-			model.add(Dense(40, input_dim=4, activation='relu'))
-			model.add(Dense(10, input_dim=4, activation='relu'))
-			model.add(Dense(10, activation='tanh'))
-			#model.add(Dense(16, activation='relu'))
-=======
 			model.add(Dense(40, input_dim=4, activation='tanh'))
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 			model.add(Dense(2, activation='linear'))
 		elif(environment_name == 'MountainCar-v0'):
 			model.add(Dense(16, input_dim=2, activation='relu'))
@@ -145,25 +138,15 @@ class Replay_Memory():
 		return [self.memory[randint(0,self.counter)] for _ in range(batch_size)]
 		# This function returns a batch of randomly sampled transitions - i.e. state, action, reward, next state, terminal flag tuples.
 		# You will feed this to your model to train.
-<<<<<<< HEAD
-		pass
-
-	def append(self, transition):
-		#transition is a 4-tuple of s,a,r,s
-=======
 
 	def append(self, transition):
 		#transition is a 5-tuple of s,a,r,s,d
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 		if self.memory is None:
 			self.memory = [transition for _ in range(self.memsize)]
 		self.memory[self.counter] = transition
 		self.counter += 1
 		if self.counter >= self.memsize:
-<<<<<<< HEAD
-=======
 			self.full = True
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 			self.counter = 0
 		# Appends transition to the memory.
 
@@ -204,12 +187,6 @@ class DQN_Agent():
 
 	def greedy_policy(self, q_values):
 		return lambda state : q_values.greedy_action(state)
-<<<<<<< HEAD
-		# Creating greedy policy for test time.
-		pass
-=======
-		# Creating greedy policy for test time.		
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 
 	def lookahead_policy(self,q_values,state):
 		best_action = 0
@@ -246,41 +223,22 @@ class DQN_Agent():
 		choosing_time = 0.
 		q_eval_time = 0.
 		fitting_time = 0.
-<<<<<<< HEAD
-		while not done:
-			self.env.render()
-=======
 
 		tot_reward = 0
 		while not done:
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 			choosing_time -= time.time()
 			action = e_greedy(state)
 			choosing_time += time.time()
 
 			old_state = state
-			state, reward, done, _ = self.env.step(action)
-<<<<<<< HEAD
-			
-			self.replay_memory.append((old_state,action,reward,state))
-=======
-
+			state, reward, done, _ = self.env.step(action)			
 			tot_reward += reward
 			
 			self.replay_memory.append((old_state,action,reward,state,done))
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 			
 			train_on = self.replay_memory.sample_batch()
 			
 			q_eval_time -= time.time()
-<<<<<<< HEAD
-			q_pairs = [(s1,a,r + self.gamma * (self.q_net.q_value(s2))) for (s1,a,r,s2) in train_on]
-			self.q_net.fit(q_pairs)
-			
-		self.epsilon -= self.epsilon_decay
-
-		pass
-=======
 			q_pairs = [(s1,a,r if d else r + self.gamma * (self.q_net.q_value(s2))) for (s1,a,r,s2,d) in train_on]
 			q_eval_time += time.time()
 
@@ -297,7 +255,6 @@ class DQN_Agent():
 		self.epsilon -= self.epsilon_decay
 
 		return tot_reward
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 
 	def test(self,episodes,model_file=None):
 		# Evaluate the performance of your agent over 100 episodes, by calculating cummulative rewards for the 100 episodes.
@@ -326,11 +283,7 @@ class DQN_Agent():
 			action = pol(state)
 			old_state = state
 			state, reward, done, _ = self.env.step(action)
-<<<<<<< HEAD
-			self.replay_memory.append((old_state,action,reward,state))
-=======
 			self.replay_memory.append((old_state,action,reward,state,done))
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 
 		# Initialize your replay memory with a burn_in number of episodes / transitions.
 
@@ -356,15 +309,6 @@ def main(args):
 	# Setting this as the default tensorflow session.
 	keras.backend.tensorflow_backend.set_session(sess)
 
-<<<<<<< HEAD
-	episodes = 1000
-	save_freq = 150
-	# You want to create an instance of the DQN_Agent class here, and then train / test it.
-	dqn = DQN_Agent('MountainCar-v0')
-	for i in range(episodes):
-		print(i)
-		dqn.train()
-=======
 	episodes = 10000
 	save_freq = 150
 	# You want to create an instance of the DQN_Agent class here, and then train / test it.
@@ -374,7 +318,6 @@ def main(args):
 		print(i)
 		rewards.append(dqn.train())
 		print("running average" + str(np.mean(rewards) if len(rewards) < 51 else rewards[-50:]))
->>>>>>> 1c9958d9b9bd836777a744086f4f19059cb21401
 		if (i + 1) % save_freq == 0:
 			dqn.q_net.save_model()
 			print("saved model after " + str(i) + " episodes.")
