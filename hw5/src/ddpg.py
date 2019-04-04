cc5b39e32344da2a7162c246a22cd1a26d3d2f86
Avi Rudich <avi.rudich@gmail.com>
2019-04-04 16:34:25 -0400

    critic learning

--- hw5/src/ddpg.py
+++ hw5/src/ddpg.py
@@ -119,9 +119,8 @@ class DDPG:
                 transitions = self.replay_memory.sample_batch()
                 augtrans = [(s1,a,r,s2,self.y_value(reward,state)) for (s1,a,r,s2) in transitions]
                 y_values = [y_value for (_,_,_,_,y_value) in augtrans]
-                states = [s1 for (s1,_,_,_) in transitions]
-                self.critic.fit(x = np.array(states),y = np.array(y_values),verbose=0,epochs=1)
-
+                state_actions = [np.concat(s1,a) for (s1,a,_,_) in transitions]
+                self.critic.fit(x = np.array(state_actions),y = np.array(y_values),verbose=0,epochs=1)
 
 
     def add_hindsight_replay_experience(self, states, actions, end_state):
