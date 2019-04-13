4ff889ec7d4e742ba15cfafeb2aa8549446fbe22
Peter Boyland <pboyland@andrew.cmu.edu>
2019-04-13 01:16:38 -0400

    fixed a ton of bugs and added a ton of command line arguments for hyperparameters\n The biggest bug was that the actor weights were being trained by the critic updates.\n You can now set time to be included in the state or not. If not, it's taken as constant.

--- hw5/src/ddpg.py
+++ hw5/src/ddpg.py
@@ -10,8 +10,9 @@ import argparse
 import gym
 import envs
 from numpy.random import randint
-import time
 import sys
+import matplotlib.pyplot as plt
+
 
 
 def concat(list1,list2):
@@ -42,67 +43,69 @@ class DDPG:
         self.verbose = args.verbose
         self.tau = args.tau
         self.gamma = args.gamma
+        self.epsilon = args.epsilon
+        self.std = args.std
+        self.regfactor = args.regularization
+
         self.env = env
 
         #creating the models
         self.actorInput,self.actorOutput,self.actorWeights = self.actor_model_init()
         self.criticInput,self.criticOutput,self.criticWeights = self.critic_model_init(self.actorOutput,self.actorInput)
-        self.actorTargetInput,self.actorTargetOutput,self.actorTargetWeights = self.actorInput,self.actorOutput,self.actorWeights #self.actor_model_init()
-        self.criticTargetInput,self.criticTargetOutput,self.criticTargetWeights = self.criticInput,self.criticOutput,self.criticWeights #self.critic_model_init(self.actorTargetOutput,self.actorTargetInput)
+        self.actorTargetInput,self.actorTargetOutput,self.actorTargetWeights = self.actor_model_init()
+        self.criticTargetInput,self.criticTargetOutput,self.criticTargetWeights = self.critic_model_init(self.actorTargetOutput,self.actorTargetInput)
 
         #creating the losses and optimizers
         self.AdamCritic = tf.train.AdamOptimizer(learning_rate = self.critic_lr)
         self.AdamActor = tf.train.AdamOptimizer(learning_rate = self.actor_lr)
 
-        self.y_value = tf.placeholder(tf.float32,shape=None)
+        self.y_value = tf.placeholder(tf.float32)
         criticLoss = tf.losses.mean_squared_error(self.y_value,self.criticOutput)
-        self.trainCritic = self.AdamCritic.minimize(criticLoss)
+        self.trainCritic = self.AdamCritic.minimize(criticLoss,var_list=self.criticWeights)
 
-        actorGradients = tf.gradients(self.criticOutput,self.actorWeights)
-        self.trainActor = self.AdamActor.apply_gradients(list(zip(actorGradients,self.actorWeights)))
+        regularizer = tf.add_n([tf.nn.l2_loss(ws) for ws in self.actorWeights])
+        actorLoss = tf.math.multiply(-1.0,self.criticOutput) + self.regfactor * regularizer
+
+        self.trainActor = self.AdamActor.minimize(actorLoss,var_list=self.actorWeights)
+        actorGradients = tf.gradients(actorLoss,self.actorOutput)
 
         #creating the sets
-        #self.copyActor = tf.assign(np.array(self.actorTargetWeights),np.array(self.actorWeights))
-        #self.copyCritic = tf.assign(self.criticTargetWeights,np.array(self.criticWeights))
+        self.copyActor = [tf.assign(tw,tw*(1.0-self.tau)+self.tau*w) for (tw,w) in zip(self.actorTargetWeights,self.actorWeights)]
+        self.copyCritic =[tf.assign(tw,tw*(1.0-self.tau)+self.tau*w) for (tw,w) in zip(self.criticTargetWeights,self.criticWeights)]
 
         #preparing to run and train
         self.replay_memory = Replay_Memory()
-        self.epsilon = 0.2
-        self.std = 0.01
 
-        '''
-        
-
-        self.value_grads = tf.gradients(self.critic.output, self.critic.input)
-        #print(self.value_grads[0][0][6:])
-        self.param_grads = tf.gradients(self.actor.output,self.actor.trainable_weights,grad_ys = self.value_grads[0][0][6:])
-
-        self.updateActor = self.Adam.apply_gradients(list(zip(self.param_grads,self.actor.trainable_weights)))
-
-        self.myprint = tf.print(self.param_grads)'''
 
         self.sess = tf.Session()
         init = tf.global_variables_initializer()
         self.sess.run(init)
 
+        #print statements
+        self.printCriticInput = tf.print(self.criticInput)
+        self.print_y_value = tf.print(self.y_value)
+        self.print_critic_output = tf.print(self.criticOutput)
+        self.print_critic_loss = tf.print(criticLoss)
+        self.print_actor_loss = tf.print(actorLoss)
+        self.printActorGrads = tf.print(actorGradients)
+
+
 
     def actor_model_init(self):
         with tf.variable_scope("actorscope"):
-            actorInput = tf.placeholder(tf.float32, shape=(None,6))
-            layer1 = Dense(400,activation='relu')(actorInput)
-            layer2 = Dense(16,activation='relu')(layer1)
+            actorInput = tf.placeholder(tf.float32, shape=(None,7))
+            layer1 = Dense(400,activation='tanh')(actorInput)
+            layer2 = Dense(400,activation='relu')(layer1)
             actorOutput = Dense(2,activation='tanh')(layer2)
-            #actorOutput = [var for var in actorOutput.output]
             actorWeights = tf.trainable_variables(scope="actorscope")
             return actorInput, actorOutput, actorWeights
 
     def critic_model_init(self,actorInput,actorOutput):
         with tf.variable_scope("criticscope"):
             criticInput = tf.concat([actorInput,actorOutput],-1)
-            layer1 = Dense(400,activation='relu')(criticInput)
-            layer2 = Dense(16,activation='relu')(layer1)
-            criticOutput = Dense(2,activation='linear')(layer2)
-            #criticOutput = [var for var in criticOutput.output]
+            layer1 = Dense(400,activation='tanh')(criticInput)
+            layer2 = Dense(400,activation='relu')(layer1)
+            criticOutput = Dense(1,activation='linear')(layer2)
             criticWeights = tf.trainable_variables(scope="criticscope")
             return criticInput, criticOutput, criticWeights
 
@@ -114,71 +117,102 @@ class DDPG:
         a = self.sess.run(self.criticTargetOutput,feed_dict={self.actorTargetInput: np.array([state])})
         return a[0][0]
 
-    def test(self, num_episodes):
+    def test(self, num_episodes,render=False,keeptime=True):
+        print("\n\n\nTESTING BEGINS HERE\n\n\n")
         rewards = []
         for _ in range(num_episodes):
             total = 0
             state = self.env.reset()
             done = False
+            objectxs = []
+            objectys = []
+            time = 0.0
             while not done:
-                action = self.choose(state)
+                objectxs.append(state[0])
+                objectys.append(state[1])
+                timestate = np.append(state,time)
+                action = self.choose(timestate)
+                print(action)
+                
                 state,reward,done,_ = self.env.step(action)
                 total += reward
+                time += 1.0
             rewards.append(total)
+            if render:
+                plt.plot(objectxs,objectys)
+                plt.show()
         return np.mean(rewards)
 
     def add_noise(self, action):
-        return action + np.random.normal(0,.01,2)
+        return action + np.random.normal(0,self.std,2)
 
     def calc_y_value(self,reward,state):
         return reward + self.gamma * self.predict_value(state)
 
-    def train(self, num_episodes, hindsight=False):
-        # Write your code here to interact with the environment.
-        # For each step you take in the environment, sample a batch
-        # from the experience replay and train both your actor
-        # and critic networks using the sampled batch.
-        #
-        # If ``hindsight'' option is specified, you will use the
-        # provided environment to add hallucinated transitions
-        # into the experience replay buffer.
+    def train(self, num_episodes, hindsight=False,keeptime=True):
         for e in range(num_episodes):
             state = self.env.reset()
             done = False
             total_reward = 0
+            time = 0.0
             while not done:
+                statetime = np.append(state,time)
                 if np.random.rand() < self.epsilon:
                     action = self.random_action()
                 else:
-                    action = self.choose(state)
+                    action = self.choose(statetime)
+                    print(action)
                     action = self.add_noise(action)
+                #print(action)
+
                 newstate,reward,done,_ = self.env.step(action)
+                reward = reward / 10.0
+
                 total_reward += reward
-                self.replay_memory.append((state,action,reward,newstate))
+                self.replay_memory.append((statetime,action,reward,np.append(newstate,time+1.0 if keeptime else time)))
+                state = newstate
 
-                for _ in range(32):
                 #setting up lists
-                    transitions = self.replay_memory.sample_batch(1)
-                    y_values = [self.calc_y_value(r,s) for (_,_,r,s) in transitions]
-                    state_actions = [concat(s1,a) for (s1,a,_,_) in transitions]
-                    states = [s for (s,_,_,_) in transitions]
+                transitions = self.replay_memory.sample_batch(32)
+                y_values = [self.calc_y_value(r,s) for (_,_,r,s) in transitions]
+                state_actions = [concat(s1,a) for (s1,a,_,_) in transitions]
+                states = [s for (s,_,_,_) in transitions]
 
-                    #update the critic and actor
-                    '''criticInput:state_actions'''
-                    #TODO: not sure if this is running correctly
-                    self.sess.run(self.trainCritic,feed_dict={self.criticInput:state_actions,self.actorInput:states, self.y_value:y_values})
-                    self.sess.run(self.trainActor,feed_dict={self.actorInput:states})
+                #print("actor loss")
+                #self.sess.run(self.print_actor_loss,feed_dict={self.actorInput:states})
+                self.sess.run(self.trainActor,feed_dict={self.actorInput:states})
+                #print("y-values")
+                #self.sess.run(self.print_y_value,feed_dict={self.criticInput:state_actions,self.actorInput:states, self.y_value:y_values})
+                #print("criticoutput")
+                #self.sess.run(self.print_critic_output,feed_dict={self.criticInput:state_actions,self.actorInput:states, self.y_value:y_values})
+                #print("loss next")
+                #self.sess.run(self.print_critic_loss,feed_dict={self.criticInput:state_actions,self.actorInput:states, self.y_value:y_values})
+                #print("actor_gradients")
+                #self.sess.run(self.printActorGrads,feed_dict={self.actorInput:states})
+                #print("")
+                self.sess.run(self.trainCritic,feed_dict={self.criticInput:state_actions, self.y_value:y_values})
+                #self.sess.run(self.printCriticInput,{self.criticInput:fake_state_actions,self.actorInput:states})
+                #self.sess.run(self.printCriticInput,{self.actorInput:states})
+                #print("")
 
-            #self.sess.run(self.copyCritic)
-            #self.sess.run(self.copyActor)
+                transitions = self.replay_memory.sample_batch(32)
+                y_values = [self.calc_y_value(r,s) for (_,_,r,s) in transitions]
+                state_actions = [concat(s1,a) for (s1,a,_,_) in transitions]
+                states = [s for (s,_,_,_) in transitions]
+                self.sess.run(self.trainCritic,feed_dict={self.criticInput:state_actions, self.y_value:y_values})
 
-            print("episode = ",e,", total reward = ",total_reward)
 
+                self.sess.run(self.copyCritic)
+                self.sess.run(self.copyActor)
+                if keeptime:
+                    time += 1.0
+
+            print("episode = ",e,", total reward = ",total_reward * 10)
 
 
     def random_action(self):
         return [x*2 - 1 for x in np.random.rand(2)]
-
+    '''
     def burn_in(self, burn=100):
         done = False
         state = self.env.reset()
@@ -188,8 +222,7 @@ class DDPG:
             action = self.random_action()
             old_state = state
             state, reward, done, _ = self.env.step(action)
-            self.replay_memory.append((old_state,action,reward,state,done))
-
+            self.replay_memory.append((old_state,action,reward,state,done))'''
     def save_models(self):
         self.save_actor_model()
         self.save_critic_model()
@@ -246,13 +279,19 @@ def parse_arguments():
     parser.add_argument('--num-episodes', dest='num_episodes', type=int,
                         default=50000, help="Number of episodes to train on.")
     parser.add_argument('--actor_lr', dest='actor_lr', type=float,
-                        default=1e-4, help="The actor's learning rate.")
+                        default=1e-4, help="The actor's learning rate.") #used to be 1e-4
     parser.add_argument('--critic_lr', dest='critic_lr', type=float,
-                        default=1e-3, help="The critic's learning rate.")
+                        default=1e-3, help="The critic's learning rate.") #used to be 1e-3
     parser.add_argument('--tau', dest='tau', type=float,
                         default=0.05, help="The rate to update the slow network.")
     parser.add_argument('--gamma', dest='gamma', type=float,
                         default=1.0, help="The decay of value rate.")
+    parser.add_argument('--epsilon', dest='epsilon', type=float,
+                        default=0.5, help="The chance of a random action.")
+    parser.add_argument('--std', dest='std', type=float,
+                        default=0.01, help="The standard deviation of noise to add.")
+    parser.add_argument('--reg', dest='regularization', type=float,
+                        default=0.01, help="The level of regularization.")
 
     parser_group = parser.add_mutually_exclusive_group(required=False)
     parser_group.add_argument('--render', dest='render',
@@ -262,6 +301,15 @@ def parse_arguments():
                               action='store_false',
                               help="Whether to render the environment.")
     parser.set_defaults(render=False)
+
+    parser_group2 = parser.add_mutually_exclusive_group(required=False)
+    parser_group2.add_argument('--time', dest='time',
+                              action='store_true',
+                              help="Whether to give the network time.")
+    parser_group2.add_argument('--no-time', dest='render',
+                              action='store_false',
+                              help="Whether to give the network time.")
+    parser.set_defaults(render=True)
 
     parser.add_argument('--save_dir',dest = 'saved_dir', type = str, default = 'models')
     parser.add_argument('--actor_file',dest='actor_file',type=str,default = 'actor_model')
@@ -282,7 +330,7 @@ def main():
     args = parse_arguments()
     env = gym.make('Pushing2D-v0')
     algo = DDPG(env, args)
-    algo.train(50000) #50000
+    algo.train(5000) #50000
     print(algo.test(100))
 
 if __name__=='__main__':
